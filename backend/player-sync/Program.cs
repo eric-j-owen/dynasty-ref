e6e46109c51ad7ce95580b2165e91f18c08849c7
eric-j-owen <ejowen+github@proton.me>
2025-10-05 13:06:24 -0400

    adds config for basic db connection, http client, and working test call to api

---
+++ backend/player-sync/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Data;
+using Data.Models; 
+
+
+
+//secrets config
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+
+//httpclient config
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/json"));
+
+//db config
+var options = new DbContextOptionsBuilder<AppDbContext>()
+    .UseNpgsql(config["ConnectionStrings:AppDbContext"])
+    .Options;
+
+
+await ProcessRepositoriesAsync(client);
+
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+    var json = await client.GetStringAsync("https://api.sleeper.app/v1/user/837121062331318272/leagues/nfl/2025");
+
+    Console.Write(json);
+}
