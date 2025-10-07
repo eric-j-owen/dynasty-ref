ba5dc90f3d8e1a65ad97eb4a81eff7e17f1c667e
eric-j-owen <ejowen+github@proton.me>
2025-10-07 16:02:15 -0400

    adds staging table for player data

--- backend/data/AppDbContext.cs
+++ backend/data/AppDbContext.cs
@@ -5,11 +5,12 @@ namespace Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) 
+        public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
         }
 
         public DbSet<Player> Players { get; set; }
+        public DbSet<PlayerStaging> PlayersStaging { get; set; }
     }
 }
