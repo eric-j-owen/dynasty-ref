a794c0ae5c8589fa3ede7836d11c07750d26b0e2
eric-j-owen <ejowen+github@proton.me>
2025-10-27 18:40:57 -0400

    organizes consts relating to api connections

--- backend/Shared/Consts/Api.cs
+++ backend/Shared/Consts/Api.cs
@@ -1,10 +1,19 @@
 namespace Shared.Consts;
 
+
 public static class Api
 {
-    public const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
 
-    public const string KtcBaseUrl = "https://keeptradecut.com/";
-    public const string FcBaseUrl = "https://api.fantasycalc.com/";
+    public static class Config
+    {
+        public const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
+    }
+
+    public static class BaseUrls
+    {
+        public const string Ktc = "https://keeptradecut.com/";
+        public const string Fc = "https://api.fantasycalc.com/";
+        public const string Sleeper = "https://api.sleeper.app/v1/";
+    }
 
 }
