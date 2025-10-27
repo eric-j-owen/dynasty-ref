d75293f70c5399c0886902b44c3ac6f4602ef21c
eric-j-owen <ejowen+github@proton.me>
2025-10-27 18:37:47 -0400

    refactors Program.cs to clean up unused code and registers services

--- backend/DataPipeline/Program.cs
+++ backend/DataPipeline/Program.cs
@@ -1,30 +1,38 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-// using System.Net.Http.Headers;
-// using Shared.Services;
+using DataPipeline.Services;
+using Db;
+using DataPipeline.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using DataPipeline.DataProviders;
-using DataPipeline.Helpers;
-using System.Runtime.CompilerServices;
-// using Shared.DTOs;
-// using WebScraping;
-// using Data.Services;
-// using Data.Models;
+using Shared.Consts;
+using DataPipeline.DataProviders.PlayerMaster;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
-// builder.Services.AddHttpClient<FcScraper>(
-//     client =>
-//     {
-//         client.BaseAddress = new Uri(Consts.FcBaseUrl);
-//         client.DefaultRequestHeaders.Accept.Add(
-//             new MediaTypeWithQualityHeaderValue("application/json"));
-//         client.DefaultRequestHeaders.Add("User-Agent", Consts.UserAgent);
-//     }
-// );
+builder.Services.AddDbContextPool<AppDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("")));
 
-builder.Services.AddTransient<KtcProvider>();
-// builder.Services.AddTransient<FileService>();
-// builder.Services.AddTransient<PlayerValueService>();
+builder.Services.AddTransient<PlayerMasterService>();
+builder.Services.AddTransient<KtcService>();
+
+//data providers
+builder.Services.AddHttpClient<ExtractSleeperPlayers>(client =>
+    {
+        client.BaseAddress = new Uri(Api.BaseUrls.Sleeper);
+        client.DefaultRequestHeaders.Add("Accept", "application/json");
+    }
+);
+
+builder.Services.AddHttpClient<KtcScraper>(client =>
+    {
+        client.BaseAddress = new Uri(Api.BaseUrls.Ktc);
+        client.DefaultRequestHeaders.Add("User-Agent", Api.Config.UserAgent);
+    }
+
+);
+
 
 using IHost host = builder.Build();
 
@@ -36,53 +44,14 @@ try
     }
 
     var arg = args[0];
-    switch (arg)
+    IDataPipelineService service = arg switch
     {
-        case Consts.DataSources.Ktc:
-            var KtcProvider = host.Services.GetRequiredService<KtcProvider>();
-            await KtcProvider.ExtractDataAsync($"{Consts.Paths.KtcBase}?page=0");
-            break;
-        default:
-            Console.WriteLine("invalid argument");
-            break;
-    }
+        "players" => host.Services.GetRequiredService<PlayerMasterService>(),
+        "ktc" => host.Services.GetRequiredService<KtcService>(),
+        _ => throw new Exception("invalid argument"),
+    };
 
-    if (args[0] == Consts.DataSources.Ktc)
-    {
-
-    }
-
-    //     if (args[0] == "push")
-    //     {
-    //         PlayerValueService PlayerValueService = host.Services.GetRequiredService<PlayerValueService>();
-    //         var data = fileService.ReadFromFileJson();
-    //         PlayerValueService.ProcessPlayerValueData();
-    //         return;
-    //     }
-    //     else
-    //     {
-    //         string fileName;
-    //         IScraper scraper;
-    //         List<ScrapedPlayer> scraped;
-    //         FileService fileService = host.Services.GetRequiredService<FileService>();
-
-    //         if (args[0] == Consts.SourceKtc)
-    //         {
-    //             fileName = Consts.SourceKtc;
-    //             scraper = host.Services.GetRequiredService<KtcScraper>();
-
-    //             scraped = await scraper.ScrapeAsync();
-    //         }
-    //         else if (args[0] == Consts.SourceFc)
-    //         {
-    //             fileName = Consts.SourceFc;
-    //             scraper = host.Services.GetRequiredService<FcScraper>();
-
-    //             scraped = await scraper.ScrapeAsync();
-    //         }
-
-    //         fileService.WriteToFileJson(fileName, scraped);
-    //     }
+    await service.RunAsync();
 }
 catch (Exception e)
 {
