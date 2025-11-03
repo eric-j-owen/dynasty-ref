a3d8ccfe52395f9b3ad59852b97f7d51ab54dead
eric-j-owen <ejowen+github@proton.me>
2025-11-03 14:48:33 -0500

    implements player pipeline service

---
+++ backend/DataPipeline/Pipelines/PlayerPipeline.cs
@@ -0,0 +1,40 @@
+using DataPipeline.DTOs;
+using DataPipeline.Interfaces;
+using Db.Models;
+using Microsoft.Extensions.Logging;
+
+namespace DataPipeline.Pipelines;
+
+public class PlayerPipeline(
+    IDataProvider<SleeperPlayer> provider,
+    IDataTransformer<SleeperPlayer> transformer,
+    IDataLoader<Player> loader,
+    ILogger<PlayerPipeline> logger) : IPipeline
+{
+    public async Task RunAsync()
+    {
+        try
+        {
+            var dataExtract = await provider.ExtractDataAsync();
+            var transformedData = transformer.Transform(dataExtract);
+
+            if (transformedData?.IncompletePlayerData?.Count > 0)
+            {
+                var count = transformedData.IncompletePlayerData.Count;
+                logger.LogWarning("found {count} incomplete data records", count);
+            }
+
+            if (transformedData?.PlayerData?.Count > 0)
+            {
+                await loader.LoadData(transformedData.PlayerData);
+            }
+
+        }
+        catch (Exception e)
+        {
+            logger.LogError("sleeperplayerpipeline failed {e}", e);
+            throw;
+        }
+    }
+
+}
