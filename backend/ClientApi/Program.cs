d3901c2bd990bfbc5c377189b2e98e829f8b2708
eric-j-owen <ejowen+github@proton.me>
2025-11-04 20:43:30 -0500

    updates cors

--- backend/ClientApi/Program.cs
+++ backend/ClientApi/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("http://localhost");
+            policy.WithOrigins("http://localhost:5173", "https://dynasty-ref.xyz");
         });
 });
 
