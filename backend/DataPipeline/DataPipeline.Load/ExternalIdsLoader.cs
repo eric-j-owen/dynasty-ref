18692b0d6a023480cbf88b416b729a2a51bdb86b
eric-j-owen <ejowen+github@proton.me>
2025-11-13 11:56:54 -0500

    updates dto

--- backend/DataPipeline/DataPipeline.Load/ExternalIdsLoader.cs
+++ backend/DataPipeline/DataPipeline.Load/ExternalIdsLoader.cs
@@ -1,12 +1,13 @@
+using DataPipeline.DTOs;
 using DataPipeline.Interfaces;
 using Db.Models;
 
 namespace DataPipeline.DataPipeline.Load;
 
 
-public class ExternalIdsLoader : IDataLoader<ExternalIdModel>
+public class ExternalIdsLoader : IDataLoader<ExternalIdWithLookupDto>
 {
-    public async Task<int> LoadData(List<ExternalIdModel> data)
+    public async Task<int> LoadData(List<ExternalIdWithLookupDto> data)
     {
         Console.WriteLine("load)");
         return await Task.FromResult(1);
