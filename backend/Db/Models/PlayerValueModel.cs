0ee707ba0097aeebe43e03e33ffa0387a6c28841
eric-j-owen <ejowen+github@proton.me>
2025-11-14 13:33:27 -0500

    updates player value model to include unique index

--- backend/Db/Models/PlayerValueModel.cs
+++ backend/Db/Models/PlayerValueModel.cs
@@ -1,9 +1,11 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 using Shared.Consts;
 namespace Db.Models;
 
 
 [Table("player_values")]
+[Index(nameof(DataSource), nameof(PprFormat), nameof(IsSuperFlex), nameof(PlayerId), nameof(CreatedAt), IsUnique = true)]
 public class PlayerValueModel
 {
     [Column("id")]
@@ -15,11 +17,14 @@ public class PlayerValueModel
     [Column("is_super_flex")]
     public required bool IsSuperFlex { get; set; }
 
+    [Column("ppr_format")]
+    public required string PprFormat { get; set; } = "0.5";
+
     [Column("value")]
     public required int Value { get; set; }
 
     [Column("created_at")]
-    public required DateTime CreatedAt { get; set; }
+    public required DateOnly CreatedAt { get; set; }
 
 
 
