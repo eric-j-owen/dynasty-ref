ba21b348a62a79f236702b7afbda497a3f7c095e
eric-j-owen <ejowen+github@proton.me>
2025-11-14 13:33:40 -0500

    cleans up logging

--- backend/DataPipeline/Pipelines/RunPipeline.cs
+++ backend/DataPipeline/Pipelines/RunPipeline.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using DataPipeline.DataPipeline.Load;
 using DataPipeline.DTOs;
 using DataPipeline.Interfaces;
 using Db.Models;
@@ -17,41 +18,46 @@ public class RunPipeline<T>(
     public async Task RunAsync()
     {
         var runner = typeof(T).Name;
+        var dataCount = (0, 0, 0); //updated, added, incomplete
+
         logger.LogInformation("beginning Pipeline for {t}.", runner);
         try
         {
             logger.LogInformation("beginning data extract");
             var dataExtract = await provider.ExtractDataAsync();
-            logger.LogInformation("extract count: {x}", dataExtract.Count);
 
             logger.LogInformation("beginning data transform");
             var transformedData = transformer.Transform(dataExtract);
 
+            logger.LogInformation("beginning data load");
+            // player loader
             if (transformedData?.PlayerData?.Count > 0)
             {
-
-                logger.LogInformation("transformed {x} records", transformedData.PlayerData.Count);
-
-                logger.LogInformation("beginning data load");
-                logger.LogInformation("found {x} records of incomplete data", transformedData.IncompletePlayerData!.Count);
-                logger.LogDebug("data {x}", JsonSerializer.Serialize(transformedData.IncompletePlayerData));
-
                 var res = await playerLoader.LoadData(transformedData.PlayerData);
-
-                logger.LogInformation("updated {x} players", res.UpdateCount);
-                logger.LogInformation("added {x} new players", res.AddCount);
+                dataCount = (res.UpdateCount ?? 0, res.AddCount ?? 0, transformedData.IncompletePlayerData!.Count);
             }
 
+            // external ids loader
             if (transformedData?.ExternalIdPlayerData?.Count > 0)
             {
-                logger.LogInformation("transformed {x} records", transformedData.ExternalIdPlayerData.Count);
-
-                logger.LogInformation("beginning data load");
                 var res = await idloader.LoadData(transformedData.ExternalIdPlayerData);
-                logger.LogInformation("added {x} new ids", res.AddCount);
+                dataCount = (0, res.AddCount ?? 0, 0);
+            }
+
+            // player values loader
+            if (transformedData?.PlayerValueData?.Count > 0)
+            {
+                // var res = PlayerValuesLoader.LoadData(transformedData.PlayerValueData);
+                // dataCount = (0, res.AddCount, 0);
+
             }
 
             logger.LogInformation("{r} success", runner);
+            Console.WriteLine("==================METRICS========================");
+            logger.LogInformation("extract count: {x}", dataExtract.Count);
+            logger.LogInformation("updated: {a}, added: {b}, incomplete: {c}", dataCount.Item1, dataCount.Item2, dataCount.Item3);
+            Console.WriteLine("=================================================");
+
 
         }
         catch (Exception e)
